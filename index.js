365e309dc9cc9996960574b424debf58ab53b5c6
Jeff Araujo <jaraujo@pipersolutions.com>
2021-10-09 00:05:37 -0500

    Terminando autentificación y acceso de servicios

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); //Sintaxis de importación en nodejs
 require('dotenv').config();
-const {dbConection} = require('./config/database')
+const { dbConection } = require('./config/database')
 const cors = require('cors');
 
 //Crear el servidor express
@@ -9,20 +9,18 @@ const app = express()
 //Configurar cors
 app.use(cors());
 
+app.use(express.json());
+
 //Estableciendo conexión a la BD
 dbConection();
 //console.log(process.env)
-//test
 
 //Rutas de la API Proyectos
-app.get('/', (req,res)=>{
-    res.status(400).json({
-        ok:true,
-        msg:'Bienvenidos a NodeJS'
-    });
-});
+app.use('/api/usuarios', require('./routes/usuarios.routes'));
+app.use('/api/login', require('./routes/auth.routes'));
+//app.use('/api/investigadores' , require('./routes/investigadores.routes'))
 
 //Codigo para desplegar el servidor
-app.listen(process.env.PORT,()=>{
-    console.log('Servidor desplegado en el puerto:'+process.env.PORT)
+app.listen(process.env.PORT, () => {
+    console.log('Servidor desplegado en el puerto:' + process.env.PORT)
 })
